835a9819b142e3e0ace4b53ee73305d5cecbf6da
llFireHawkll <sparsh.dtt@gmail.com>
2020-08-27 00:14:35 +0530

    initializing folder structure

---
+++ pycognitio/__init__.py
@@ -0,0 +1,12 @@
+'''
+File: __init__.py
+Project: pycognitio
+File Created: Thursday, 27th August 2020 12:13:09 am
+Author: Sparsh Dutta (sparsh.dtt@gmail.com)
+-----
+Last Modified: Thursday, 27th August 2020 12:14:34 am
+Modified By: Sparsh Dutta (sparsh.dtt@gmail.com>)
+-----
+Copyright 2020 Sparsh Dutta
+'''
+__version__ = '0.0.1'
